a29bb6bef79080f1f725887620c4fae1cfc9cbf1
Jirawat <jirawat.y@merudy.com>
2022-09-06 23:37:17 +0700

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 
 let PORT = process.env.PORT || 6666;
 
-//เอาไว้เข้ารหัส password
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// //เอาไว้เข้ารหัส password
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
 
 // jwt json web token เอาไว้สำหรับ object แปลงเป็น token
 var jwt = require('jsonwebtoken');
@@ -31,13 +31,14 @@ app.post('/login', jsonParser, async function (req, res) {
       const users = database.collection('users');
       const user = await users.findOne({ email: req.body.email });
       if (user) {
-         bcrypt.compare(req.body.password, user.password, function (err2, isLogin) {
-            if (isLogin) {
-               res.json({ status: true, message: 'login success', token: jwt.sign(user, secret, { expiresIn: '1h' }) });
-            } else {
-               res.json({ status: false, message: 'login Fail' });
-            }
-         });
+         res.json({ status: true, message: 'login success', token: jwt.sign(user, secret, { expiresIn: '1h' }) });
+         //  bcrypt.compare(req.body.password, user.password, function (err2, isLogin) {
+         //     if (isLogin) {
+         //        res.json({ status: true, message: 'login success', token: jwt.sign(user, secret, { expiresIn: '1h' }) });
+         //     } else {
+         //        res.json({ status: false, message: 'login Fail' });
+         //     }
+         //  });
       } else {
          res.json({ status: false, message: 'user not found.' });
       }
@@ -70,31 +71,31 @@ app.post('/user', jsonParser, async function (req, res) {
    }
 });
 
-app.post('/register', jsonParser, (req, res) => {
-   bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
-      if (err) {
-         res.json({ status: false, message: err });
+app.post('/register', jsonParser, async (req, res) => {
+   //    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+   //       if (err) {
+   //          res.json({ status: false, message: err });
+   //       } else {
+   try {
+      const users = database.collection('users');
+      const user = await users.findOne({ email: req.body.email });
+      if (user) {
+         res.json({ status: false, message: 'email duiplicate' });
       } else {
-         try {
-            const users = database.collection('users');
-            const user = await users.findOne({ email: req.body.email });
-            if (user) {
-               res.json({ status: false, message: 'email duiplicate' });
-            } else {
-               const insert_user = await users.insertOne({
-                  email: req.body.email,
-                  fname: req.body.fname,
-                  lname: req.body.lname,
-                  password: hash,
-                  create_date: new Date(),
-               });
-               res.json({ status: true, message: 'ok', data: insert_user });
-            }
-         } catch (err) {
-            res.json({ status: false, message: err });
-         }
+         const insert_user = await users.insertOne({
+            email: req.body.email,
+            fname: req.body.fname,
+            lname: req.body.lname,
+            password: hash,
+            create_date: new Date(),
+         });
+         res.json({ status: true, message: 'ok', data: insert_user });
       }
-   });
+   } catch (err) {
+      res.json({ status: false, message: err });
+   }
+   //       }
+   //    });
 });
 
 app.listen(PORT, function (req, res) {
