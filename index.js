61e7b76d24ea7cbf343ef0722a321e516010cf44
Jirawat <jirawat.y@merudy.com>
2022-09-06 22:59:37 +0700

    update

--- index.js
+++ index.js
@@ -1,70 +1,97 @@
 var express = require('express');
 var app = express();
 
-app.use(function (req, res, next) {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-   res.setHeader('Access-Control-Allow-Credentials', true);
-   next();
-});
-
 let PORT = process.env.PORT || 6666;
 
-//environment variables (เรียกไฟล์ .env)
-require('dotenv').config();
+//เอาไว้เข้ารหัส password
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// jwt json web token เอาไว้สำหรับ object แปลงเป็น token
+var jwt = require('jsonwebtoken');
+var secret = 'shhhhh';
 
 // เอาไว้รับค่า json
 var bodyParser = require('body-parser');
 // create application/json parser
 var jsonParser = bodyParser.json();
 
-//database connection
-var mongoose = require('mongoose');
-const { Schema } = mongoose;
-mongoose.Promise = global.Promise;
-const uri = process.env.ATLAS_URI;
-const options = {
-   //    autoIndex: false, // Don't build indexes
-   //    maxPoolSize: 10, // Maintain up to 10 socket connections
-   //    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-   //    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-   //    family: 4, // Use IPv4, skip trying IPv6
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-};
-mongoose.connect(uri, options);
-const connection = mongoose.connection;
-connection.once('open', () => {
-   console.log('Connected Database Successfully');
+const { MongoClient } = require('mongodb');
+// Replace the uri string with your connection string.
+const uri = 'mongodb+srv://root:Rmutpcpe1@cluster0.b23ov.mongodb.net?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+const database = client.db('resume');
+
+app.get('/', (req, res) => {
+   res.send('Hello, I am Working');
 });
 
-app.get('/user', jsonParser, function (req, res) {
-   const customerSchema = new Schema({
-      name: {
-         type: String,
-         //  required: 'Please supply a name',
-         trim: true,
-         required: true,
-      },
-      //   email: {
-      //      type: String,
-      //      unique: true,
-      //      lowercase: true,
-      //      trim: true,
-      //      required: 'Please Supply an email address',
-      //   },
-   });
-   module.exports = mongoose.models.users || mongoose.model('users', customerSchema);
-   module.exports.find({}, function (err, docs) {
+app.post('/login', jsonParser, async function (req, res) {
+   try {
+      const users = database.collection('users');
+      const user = await users.findOne({ email: req.body.email });
+      if (user) {
+         bcrypt.compare(req.body.password, user.password, function (err2, isLogin) {
+            if (isLogin) {
+               res.json({ status: true, message: 'login success', token: jwt.sign(user, secret, { expiresIn: '1h' }) });
+            } else {
+               res.json({ status: false, message: 'login Fail' });
+            }
+         });
+      } else {
+         res.json({ status: false, message: 'user not found.' });
+      }
+   } catch (err) {
+      res.json({ status: false, message: err });
+   }
+});
+
+app.post('/user', jsonParser, async function (req, res) {
+   try {
+      const token = req.headers.authorization;
+      if (token) {
+         const decoded = jwt.verify(token.split(' ')[1], secret);
+         const data = {
+            id: decoded.id,
+            email: decoded.email,
+            fname: decoded.fname,
+            lname: decoded.lname,
+         };
+         res.json({ status: true, message: 'ok', data });
+      } else {
+         res.json({ status: false, message: 'Token is not define' });
+      }
+   } catch (err) {
+      if (err.name == 'TokenExpiredError') {
+         res.json({ status: false, message: 'Token expired' });
+      } else {
+         res.json({ status: false, message: err.message });
+      }
+   }
+});
+
+app.post('/register', jsonParser, (req, res) => {
+   bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
       if (err) {
-         console.log(err);
          res.json({ status: false, message: err });
       } else {
-         if (docs == null) {
-            res.json({ status: false, message: 'Data not found' });
-         } else {
-            res.json({ status: true, message: 'success', data: docs });
+         try {
+            const users = database.collection('users');
+            const user = await users.findOne({ email: req.body.email });
+            if (user) {
+               res.json({ status: false, message: 'email duiplicate' });
+            } else {
+               const insert_user = await users.insertOne({
+                  email: req.body.email,
+                  fname: req.body.fname,
+                  lname: req.body.lname,
+                  password: hash,
+                  create_date: new Date(),
+               });
+               res.json({ status: true, message: 'ok', data: insert_user });
+            }
+         } catch (err) {
+            res.json({ status: false, message: err });
          }
       }
    });
